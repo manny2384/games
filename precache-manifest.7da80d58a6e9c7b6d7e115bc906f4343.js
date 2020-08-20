2c77d12656ee5b57438395345f0ac0b2685b83da
manny2384 <gonzalezeh2015@gmail.com>
2020-08-20 09:49:41 -0700

    Updates

---
+++ precache-manifest.7da80d58a6e9c7b6d7e115bc906f4343.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19505f6416d10b19884ead9dd96bfb79",
+    "url": "/manny2384games.github.io/index.html"
+  },
+  {
+    "revision": "ede94d821932c0fd87dd",
+    "url": "/manny2384games.github.io/static/css/main.04d37b99.chunk.css"
+  },
+  {
+    "revision": "11f30ab5c59614b75df5",
+    "url": "/manny2384games.github.io/static/js/2.9c9cc210.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/manny2384games.github.io/static/js/2.9c9cc210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede94d821932c0fd87dd",
+    "url": "/manny2384games.github.io/static/js/main.67ec05fa.chunk.js"
+  },
+  {
+    "revision": "b79bf200786ef8beb989",
+    "url": "/manny2384games.github.io/static/js/runtime-main.8658f5c6.js"
+  },
+  {
+    "revision": "4a134fa313ae282aaba8fe227ab1bbea",
+    "url": "/manny2384games.github.io/static/media/img.4a134fa3.jpg"
+  },
+  {
+    "revision": "b33be728b8781df9b52083733b15fe36",
+    "url": "/manny2384games.github.io/static/media/logo.b33be728.jpg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/manny2384games.github.io/precache-manifest.7da80d58a6e9c7b6d7e115bc906f4343.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/manny2384games.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
